a853e218a735f066ed32409b7946bf9301dc5bc1
Keri Carpenter <keri.carpenter@sony.com>
2022-06-20 23:00:56 -0700

    remarkable not resolving

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Remarkable from 'remarkable';
 import reportWebVitals from './reportWebVitals';
+
+class A extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value : 'Hello ***world!***!'};
+    this.md = new Remarkable();
+    this.b = this.b.bind(this);
+  }
+
+  b(e) {
+    this.setState(
+      {value: e.target.value}
+      );
+  }
+
+  c() {
+    console.log("It got in here!");
+    return;
+  }
+
+  render() {
+    return(
+    <div>
+    <h3>The Markdown Input is:</h3>
+    <label htmlFor='markdown-content'>Enter some markdown here</label>
+    <br></br>
+    <textarea
+      defaultValue={this.state.value}
+      onChange={this.b}
+      id='markdown-content'
+    >
+    </textarea>
+    <h3>Output of the markdown is:</h3>
+    <div
+      className='content'
+      // dangerouslySetInnerHTML={this.state.value}
+    >{this.state.value}
+    </div>
+    </div>
+    )
+  }
+
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <A />
   </React.StrictMode>
 );
 
